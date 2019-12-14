a94f43f0652dc200816bc13267b38726c990970f
Thiago Machado <thiago.machado@labbo.com.br>
2019-12-14 16:14:45 -0300

    Ajuste Skeletom

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit66d400e6d4d707ca3d0b7cf1f416f8c5::getLoader();
+return ComposerAutoloaderInit3e4f4720d77775de58e3e3ea768108e5::getLoader();
